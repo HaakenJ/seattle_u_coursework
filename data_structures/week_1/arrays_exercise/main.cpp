8fee99b60722ad9877ea9bd0c4a1eca9a7fdf5a5
Haakasaurus <kramerhjohnson@gmail.com>
2020-09-10 20:36:19 -0700

    Make makeArray function and added class summary

--- data_structures/week_1/arrays_exercise/main.cpp
+++ data_structures/week_1/arrays_exercise/main.cpp
@@ -9,9 +9,20 @@ void printArray(int *x, int count) {
     }
 }
 
+// The star lets it known that it will return a pointer to an array
+int *makeArray(int count) {
+    // You need to use new or the array will be deleted at the end of this
+    // function's scope
+    int *x = new int[count];
+    for (int i = 0; i < count; i++) {
+        cin >> x[i];
+    }
+    return x;
+}
+
 int main() {
     const int SIZE = 3;
-    int x[SIZE];
+    int *x;
     cout << "Enter 3 integers (sep. by spaces): ";
     for (int i = 0; i < SIZE; i+=) {
         cin >> x[i];
@@ -20,5 +31,9 @@ int main() {
         cout << "x[" << i << "]: " << x[i] << " ";
     }
     cout << endl;
+
+    // Since you used 'new' to create the array you must delete it from the heap
+    // Without this you would have a memory leak
+    delete[] x;
     return 0;
 }
