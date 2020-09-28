522c963d5c26f4779c54ea1266e4a6b660e20f7c
Haakasaurus <kramerhjohnson@gmail.com>
2020-09-28 14:16:42 -0700

    Completed P2:L2

--- data_structures/homework/L2/main.cpp
+++ data_structures/homework/L2/main.cpp
@@ -5,70 +5,74 @@
 
 using namespace std;
 
-void testFileOpen(string filepath) {
-    const string FILENAME = filepath;
-    string name;
-    ifstream inFile;
-
-    // now open and read from file (strings)
-    inFile.open(filepath);
-    if (inFile) {
-           while (getline(inFile, name))
-              cout << name << endl;
-        } else {
-           cout << "ERROR: cannot open file.\n";
-        }
-    inFile.close();
-}
-
 void loadExternalData(const string &filepath) {
     string *members;
     int numMembers = 0;
     int **ratings;
-    int numRatings = 0;
+    int numBooks;
 
-    // Allocate memory
+    // Allocate memory for 2D array
     members = new string[100];
     ratings = new int*[100];
     for (int i = 0; i < 100; i++)
         ratings[i] = new int[100];
 
-    // Open and read from members
     string name;
-    string nums;
+    string memberRatings;
     ifstream inFile;
-    cout << filepath << endl;
+
+    // Attempt to open file
     inFile.open(filepath);
+
     if (inFile.is_open()) {
+        // Proceed while lines are being read from file
         while (getline(inFile, name)) {
             members[numMembers] = name;
-            if (getline(inFile, nums)) {
-                istringstream ss(nums);
-                numRatings = 0;
+
+            // After a name is read, next line is the member's book ratings
+            // Read from this next line
+            if (getline(inFile, memberRatings)) {
+                istringstream ss(memberRatings);
+                numBooks = -1;
+
+                // While there is data in the input stream
                 while (ss) {
                     int rating;
                     ss >> rating;
-                    ratings[numMembers][numRatings++] = rating;
+                    // Assign the rating to the associated member's
+                    // rating array
+                    ratings[numMembers][++numBooks] = rating;
                 }
                 cout << endl;
             }
             numMembers++;
         }
     } else  {
+        // Filepath was invalid, exit application
         cout << "ERROR: cannot open file";
+        return;
     }
-    cout << numRatings << endl;
+
+    cout << "# of members: " << numMembers << endl;
+    cout << "# of books: " << numBooks << endl;
+    cout << endl;
+
+    // Display members and associated ratings
     for (int i = 0; i < numMembers; i++) {
         cout << members[i] << endl;
-        for (int j = 0; j < numRatings; j++) {
+        for (int j = 0; j < numBooks; j++) {
             cout << ratings[i][j] << " ";
         }
         cout << endl;
     }
+    // Close file
     inFile.close();
 }
 
 int main() {
-    loadExternalData(R"(D:\Haake\Documents\seattle_u_notes\data_structures\homework\L2\ratings.txt)");
+    // Needing to use ../ because Clion's cwd is the location of the .cmake
+    // file which is in the Project/CMakeFiles.  The application files along
+    // with my rating file are all included in Project/
+    loadExternalData("../ratings.txt");
     return 0;
 }
