b37f5e874204ca914100294536f3f10370c7abf6
Haakasaurus <kramerhjohnson@gmail.com>
2020-10-05 14:46:13 -0700

    Implemented addMember of p3

--- data_structures/homework/p3/Member.cpp
+++ data_structures/homework/p3/Member.cpp
@@ -48,3 +48,42 @@ Member& Member::operator=(const Member &rhs) {
 Member::~Member() {
     delete [] memberArray;
 }
+
+int Member::addMember(std::string memberName) {
+    // Determine if resize is needed
+    if (count >= capacity)
+        resize();
+
+    // Create a new MemberInfo for the new member
+    MemberInfo newMember = {count + 1000, memberName, false};
+
+    // Add new member and increase count
+    memberArray[count++] = newMember;
+
+    // Return the memberId (index)
+    return count - 1;
+}
+
+std::string Member::findName(int memberId) {
+    return std::__cxx11::string();
+}
+
+bool Member::login(int memberId) {
+    return false;
+}
+
+void Member::logout(std::string memberName) {
+
+}
+
+void Member::printAccount(int memberId) {
+
+}
+
+void Member::print() {
+
+}
+
+int Member::size() {
+    return 0;
+}
